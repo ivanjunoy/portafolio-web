ae5b6b4e6cda2c9179e51d6b797a6e9a1da7d053
Ivan Junoy <ivanjunoy@gmail.com>
2024-02-29 11:36:39 -0300

    Updates

---
+++ precache-manifest.0a59097d845c9dcdb54bd5c136918d64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801acd83a70aea820d8b28b6339b4175",
+    "url": "/portafolio-web/index.html"
+  },
+  {
+    "revision": "58dab2374bde252047a1",
+    "url": "/portafolio-web/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/portafolio-web/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/portafolio-web/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "58dab2374bde252047a1",
+    "url": "/portafolio-web/static/js/main.75a09d11.chunk.js"
+  },
+  {
+    "revision": "9a5ad682478df86d4621",
+    "url": "/portafolio-web/static/js/runtime~main.f6cccbac.js"
+  }
+]);

16ca3451922d9b221877c5511390df7301a63e39
Ivan Junoy <ivanjunoy@gmail.com>
2024-03-02 15:26:22 -0300

    add profile picture buttom

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Resume from './Components/Resume/Resume';
 import Experience from './Components/Experience/Experience';
 import Formation from './Components/Formation/Formation';
 import Skills from './Components/Skills/Skills';
-import Projects from './Projects/Projects';
+import Projects from './Components/Projects/Projects';
 import Contact from './Components/Contact/Contact';
 
 function App() {
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import profileImg from "../../Imagenes/perfil2.png"
+import "./header.css"
 
 const Header = () => {
+    const [isImageVisible, setIsImageVisible] = useState(false);
+
+    const showProfilePicture = () => {
+        setIsImageVisible(!isImageVisible);
+    };
+
     return (
         <>
             <h1>Ivan Junoy</h1>
+            <p className='profileButon' onClick={showProfilePicture}>Profile picture</p>
+            <img src={profileImg} alt="Perfil Foto" style={{ display: isImageVisible ? 'block' : 'none' }}/>
+            <hr />
         </>
     )
 }

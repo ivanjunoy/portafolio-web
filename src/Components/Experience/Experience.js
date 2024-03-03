dda0208d43737877cdc494a49e3c3ffb8d417f62
Ivan Junoy <ivanjunoy@gmail.com>
2024-03-03 14:55:45 -0300

    add url of workplaces

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -7,21 +7,21 @@ const Experience = () => {
             <div>
                 <h4>Software Engineer</h4>
                 <ul>
-                    <li>Cencosud S.A. </li>
+                    <li><a href="https://www.cencosud.com/" target="_blank" rel="noopener noreferrer">Cencosud S.A.</a></li>
                     <li>jan. 2023 - present</li>
                 </ul>
             </div>
             <div>
                 <h4>Software Development Analyst</h4>
                 <ul>
-                    <li>NEORIS</li>
+                    <li><a href="https://neoris.com/home" target="_blank" rel="noopener noreferrer">NEORIS</a></li>
                     <li>sept. 2022 - jan. 2023</li>
                 </ul>
             </div>
             <div>
                 <h4>Frontend Developer</h4>
                 <ul>
-                    <li>Fantommers</li>
+                    <li><a href="https://www.fantommers.com/" target="_blank" rel="noopener noreferrer">Fantommers</a></li>
                     <li>mar. 2022 - sept. 2022</li>
                 </ul>
             </div>

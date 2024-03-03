c217e955f9a06263c53f7993eacd9112a25c5285
Ivan Junoy <ivanjunoy@gmail.com>
2024-03-03 00:34:49 -0300

    add typewritter effect

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import profileImg from "../../Imagenes/perfil2.png"
 import "./header.css"
+import Typewriter from 'typewriter-effect';
+
+
 
 const Header = () => {
     const [isImageVisible, setIsImageVisible] = useState(false);
@@ -12,7 +15,16 @@ const Header = () => {
 
     return (
         <>
-            <h1>Ivan Junoy</h1>
+            <h1>
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                        .typeString('Ivan Junoy')
+                        .start()
+                    }}
+                />
+            </h1>
+
             <p className='profileButon' onClick={showProfilePicture}>{buttonVar}</p>
             <img className='profilePicture' src={profileImg} alt="Perfil Foto" style={{ display: isImageVisible ? 'block' : 'none' }} onClick={showProfilePicture}/>
             <hr />

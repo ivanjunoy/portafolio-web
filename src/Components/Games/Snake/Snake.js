50c9da35c8aa287f30dbd3bdeb9ed7fc073a42b4
Ivan Junoy <ivanjunoy@gmail.com>
2025-10-20 22:58:22 -0300

    snake page v2

--- src/Components/Games/Snake/Snake.js
+++ src/Components/Games/Snake/Snake.js
@@ -1,12 +1,26 @@
+import styles from "./snake.module.css";
 
 const Snake = () => {
 
+    // Crea el arreglo de divs.
+    function createArrayDiv(index) {
+        let unorderArray = [];
+        for (let n = 0; n < index; n++) {
+            unorderArray.push(
+                <div key={n} className={styles.arrayItem}>{n}</div>
+            );
+        }
 
+        return unorderArray;
+    }
+
+    const showArr = createArrayDiv(200)
+    
     return (
         <div>
-            <h1>Snake</h1>
-            <div>
-                game
+            <h1>Array:</h1>
+            <div className={styles.arrContainer}>
+                {showArr}
             </div>
         </div>
     )
